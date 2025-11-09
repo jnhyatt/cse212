c133ef5e1b01a6201d72972519135a42c9c20dea
JoshValjosh <joshua.n.hyatt@gmail.com>
2025-11-08 20:48:39 -0700

    Week 2 stuff

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,50 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Push three items with different priorities out of priority order and pop them all off.
+    // Expected Result: Items come off in order from highest priority to lowest priority.
+    // Defect(s) Found: high priority task not being dequeued after call to Dequeue. low priority task dequeued before medium priority task.
+    public void TestPriorityQueue_PopHighPriorityFirst()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("LowPriorityTask", 1);
+        priorityQueue.Enqueue("HighPriorityTask", 10);
+        priorityQueue.Enqueue("MediumPriorityTask", 5);
+        Assert.AreEqual("HighPriorityTask", priorityQueue.Dequeue());
+        Assert.AreEqual("MediumPriorityTask", priorityQueue.Dequeue());
+        Assert.AreEqual("LowPriorityTask", priorityQueue.Dequeue());
+
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: Push items with the same priority and pop them all off.
+    // Expected Result: Items come off in the same order they were added (FIFO).
+    // Defect(s) Found: tasks coming off in reverse order.
+    public void TestPriorityQueue_SamePriorityFifo()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("FirstTask", 5);
+        priorityQueue.Enqueue("SecondTask", 5);
+        Assert.AreEqual("FirstTask", priorityQueue.Dequeue());
+        Assert.AreEqual("SecondTask", priorityQueue.Dequeue());
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Pop empty queue.
+    // Expected Result: Exception is thrown.
+    // Defect(s) Found:
+    public void TestPriorityQueue_PopEmptyQueue()
+    {
+        var priorityQueue = new PriorityQueue();
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Expected an exception when dequeuing from an empty queue.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+    }
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: underlying queue returns elements in LIFO order instead of FIFO order.
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +85,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: implementation removes players with less than one turn instead of persisting infinitely.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +116,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: implementation removes players with less than one turn instead of persisting infinitely.
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
